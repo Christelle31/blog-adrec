00fb822d3d867cac5396de40b4a79f8081fe451b
Benjamin Georgeault <benjamin@pressop.eu>
2019-02-05 11:49:46 +0100

    Add example fos Entity validation.

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
@@ -25,6 +26,13 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=150)
+     *
+     * @Assert\NotBlank(message="article.title.NotBlank.message")
+     * @Assert\Length(
+     *     min=8,
+     *     max=150,
+     *     minMessage="article.title.Length.minMessage"
+     * )
      */
     private $title;
 
